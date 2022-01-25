e6a190605997499b9e5fb984abfbb78008d9b551
Rohit Patil <74045753+rohitpatilweb@users.noreply.github.com>
2022-01-26 00:12:43 +0530

    Add files via upload
    initia commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const TextToDisplay =
+  "https://api.thingspeak.com/channels/1539550/feeds.json?results=1";
+const DisplayText = document.getElementById("TextToShow");
+
+function getText() {
+  return fetch(TextToDisplay)
+    .then((response) => response.json())
+    .then((data) => data.feeds[0].field1);
+}
+
+let spacechar = document.createElement("span");
+spacechar.innerText = " ";
+async function getNextLine() {
+  const FetchedLine = await getText();
+  console.log(FetchedLine);
+  DisplayText.innerHTML = String(FetchedLine);
+}
+getNextLine();
